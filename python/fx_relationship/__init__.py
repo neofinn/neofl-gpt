834b3c5c15186c4d3d430b39f79325540f19fcd7
neofinn <sksatish3027@gmail.com>
2026-08-19 03:04:45 +0530

    Add FX Relationship Brain package

---
+++ python/fx_relationship/__init__.py
@@ -0,0 +1,5 @@
+"""NeoFL FX Relationship and Synthetic Cross Brain."""
+from .brain import FXRelationshipBrain
+from .models import FXQuote, SyntheticCross, RelationshipObservation
+
+__all__ = ["FXRelationshipBrain", "FXQuote", "SyntheticCross", "RelationshipObservation"]
