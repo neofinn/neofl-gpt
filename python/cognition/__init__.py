c83847d24db868fb6a0c4ee4f1a662ca22c5cc70
neofinn <sksatish3027@gmail.com>
2026-08-19 03:21:09 +0530

    Add NeoFL cognitive LLM layer

---
+++ python/cognition/__init__.py
@@ -0,0 +1,4 @@
+"""LLM-assisted cognitive layer for NeoFL."""
+from .contracts import CognitiveRequest, CognitiveResponse, ModelRole
+
+__all__ = ["CognitiveRequest", "CognitiveResponse", "ModelRole"]
