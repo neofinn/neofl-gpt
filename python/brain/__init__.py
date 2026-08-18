31399f55fd1f4773f22c7b87c7c7e547991b045d
neofinn <sksatish3027@gmail.com>
2026-08-19 02:07:00 +0530

    Register index specialist with NeoFL brain layer

---
+++ python/brain/__init__.py
@@ -0,0 +1,6 @@
+"""NeoFL shared brain layer."""
+
+from .index_specialist import IndexSpecialistBrain, IndexBrainResult
+from .world import IndexWorld
+
+__all__ = ["IndexSpecialistBrain", "IndexBrainResult", "IndexWorld"]
